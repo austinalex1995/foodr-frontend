501e1f7ffbaf0a70b93dcf78b1382d1fe2e8ad53
austinalex1995 <aealex1995@gmail.com>
2023-04-07 17:23:19 -0400

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoriteRecipeComponent } from './components/favorite-recipe/favorite-recipe.component';
+import { CreateRecipeComponent } from './pages/create-recipe/create-recipe.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyRecipesComponent } from './pages/my-recipes/my-recipes.component';
+import { RecipeSwipeComponent } from './pages/recipe-swipe/recipe-swipe.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/signup',
+    component: SignupComponent
+  },
+  {
+    path: '/recipes',
+    component: RecipeSwipeComponent
+  },
+  {
+    path: '/create_recipe',
+    component: CreateRecipeComponent
+  },
+  {
+    path: '/favorite_recipes',
+    component: FavoriteRecipeComponent
+  },
+  {
+    path: '/my_recipes',
+    component: MyRecipesComponent
+  },
+  {
+    path: '/settings',
+    component: SettingsComponent
+  },
+  {
+  path: '**',
+  component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
